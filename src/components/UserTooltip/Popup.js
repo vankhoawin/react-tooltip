5179f87c810803d7b960e1784f7642882dfad38c
Van Nguyen <vnguyen94@gmail.com>
2017-11-02 22:21:15 -0700

    fix Popup, add propTypes

--- src/components/UserTooltip/Popup.js
+++ src/components/UserTooltip/Popup.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import styles from './styles.scss';
 
 
-const Popup = (userData) => (
+const propTypes = {
+  userData: PropTypes.object.isRequired,
+};
+
+const Popup = ({ userData }) => (
   <div className={styles.tooltip}>
-    <h4 className={styles.title}>ID {userData.id} is {userData.type} {userData.name}</h4>
+    <h4 className={styles.title}>ID {userData.id} is {userData.type} {userData.body}</h4>
     <p className={styles.response}>API Response: {JSON.stringify(userData)}</p>
     <p className={styles.notice}>
       This tooltip only makes a request the first time. (see console)
     </p>
   </div>
 );
+
+Popup.propTypes = propTypes;
 
 
 export default Popup;
